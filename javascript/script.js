62dec36ae8224af855b3769e687bb9025677f770
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-05-23 14:29:08 +0530

    Add input form and file structure, and linter setup

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+const addBookForm = document.getElementById('book-add');
+
+const inputTitle = document.getElementById('book-title');
+const inputAuthor = document.getElementById('book-author');
+
+const addBookBtn = document.getElementById('add-btn');
+
+const submitForm = (event) => {
+    console.log(event);
+    event.preventDefault();
+}
+
+addBookForm.addEventListener('submit', )
