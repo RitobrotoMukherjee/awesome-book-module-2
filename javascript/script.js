c1c9539fde285232a5a82b6d49f86231c9c85f23
SirriCelles <sirricelles@gmail.com>
2022-05-25 10:36:16 +0100

    add page navigation via spa

--- javascript/script.js
+++ javascript/script.js
@@ -3,9 +3,45 @@ import { bookStore } from './books/bookObj.js';
 const inputTitle = document.getElementById('book-title');
 const inputAuthor = document.getElementById('book-author');
 
-const addBookBtn = document.getElementById('#add-btn');
+const addBookBtn = document.getElementById('add-btn');
 
 const removeBtn = document.querySelectorAll('.removeBtn');
+
+const listLink = document.querySelector("a[href='#book-list-section']");
+
+const addLink = document.querySelector("a[href='#add-books']");
+
+const contactLink = document.querySelector("a[href='#contact-info-section']");
+
+const listBookPage = document.querySelector('#book-list-section');
+const addBooksPage = document.querySelector('#add-books');
+const contactPage = document.querySelector('#contact-info-section');
+
+// const toggleClass = (element, claz) => {
+//   if (element.classList.contains(claz)) {
+//     element.classList.remove(claz);
+//   } else {
+//     element.classList.add(claz);
+//   }
+// };
+
+addLink.addEventListener('click', () => {
+  listBookPage.classList.add('display-none');
+  addBooksPage.classList.remove('display-none');
+  contactPage.classList.add('display-none');
+});
+
+contactLink.addEventListener('click', () => {
+  listBookPage.classList.add('display-none');
+  addBooksPage.classList.add('display-none');
+  contactPage.classList.remove('display-none');
+});
+
+listLink.addEventListener('click', () => {
+  listBookPage.classList.remove('display-none');
+  addBooksPage.classList.add('display-none');
+  contactPage.classList.add('display-none');
+});
 
 const updateBooks = () => {
   const title = inputTitle.value;
@@ -13,11 +49,11 @@ const updateBooks = () => {
 
   const errorShow = document.querySelector('.error');
 
-  if (title !== '' && author !== '') {
-    errorShow.classList.add('display-none');
-    bookStore.addBook(title, author);
+  if (title === '' || author === '') {
+    return errorShow.classList.remove('display-none');
   }
-  return errorShow.classList.remove('display-none');
+  bookStore.addBook(title, author);
+  return errorShow.classList.add('display-none');
 };
 
 addBookBtn.addEventListener('click', updateBooks);
