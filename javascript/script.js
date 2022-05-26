b7780d55fedc1d3ea3f05c3f4e772cfab88760de
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-05-26 20:15:18 +0530

    Make JS to follow DRY principle, and update style css

--- javascript/script.js
+++ javascript/script.js
@@ -4,7 +4,6 @@ const bookStore = new BookStore();
 
 // Only to show current date and time
 const today = new Date();
-document.getElementById('date-time').innerText = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()} ${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
 
 const inputTitle = document.getElementById('book-title');
 const inputAuthor = document.getElementById('book-author');
@@ -21,59 +20,28 @@ const listBookPage = document.querySelector('#book-list-section');
 const addBooksPage = document.querySelector('#add-books');
 const contactPage = document.querySelector('#contact-info-section');
 
-addLink.addEventListener('click', () => {
-  addBooksPage.style = 'display: block';
-  listBookPage.style = 'display: none';
-  contactPage.style = 'display: none';
-});
-
-contactLink.addEventListener('click', () => {
-  listBookPage.style = 'display: none';
-  addBooksPage.style = 'display: none';
-  contactPage.style = 'display: flex';
-});
-
 // create HTML from fragment
 const createHTML = () => {
   const docRange = document.createRange();
   const listWrapper = document.getElementById('book-list-wrapper');
   listWrapper.innerHTML = '';
   listWrapper.append(docRange.createContextualFragment(getHTML(bookStore.books)));
+
+  // Add event listener to the newly created button after 500 mili sec
+  setTimeout(() => {
+    // Get new buttons
+    const removeBtn = document.querySelectorAll('.removeBtn');
+    // recursively add event listener to the new buttons;
+    removeBtn.forEach((item) => {
+      item.addEventListener('click', (ev) => {
+        const bookIndex = ev.target.getAttribute('data-book-index');
+        bookStore.removeBook(bookIndex);
+        // after removing book recreate HTML
+        createHTML();
+      });
+    });
+  }, 500);
 };
-
-const removeBookCallback = (ev) => {
-  const bookIndex = ev.target.getAttribute('data-book-index');
-  bookStore.removeBook(bookIndex);
-  // after removing book recreate HTML
-  createHTML();
-
-  // Get new remove buttons
-  const removeBtn = document.querySelectorAll('.removeBtn');
-
-  // recursively add event listener to the new buttons;
-  removeBtn.forEach((item) => {
-    item.addEventListener('click', removeBookCallback);
-  });
-};
-
-const addEventListenerToEachButton = () => {
-  const removeBtn = document.querySelectorAll('.removeBtn');
-  removeBtn.forEach((item) => {
-    item.addEventListener('click', removeBookCallback);
-  });
-};
-
-const createBookList = () => {
-  createHTML();
-  addEventListenerToEachButton();
-};
-
-listLink.addEventListener('click', () => {
-  listBookPage.style = 'display: block';
-  addBooksPage.style = 'display: none';
-  contactPage.style = 'display: none';
-  createBookList();
-});
 
 const updateBooks = () => {
   const title = inputTitle.value;
@@ -85,12 +53,34 @@ const updateBooks = () => {
     return errorShow.classList.remove('display-none');
   }
   errorShow.classList.add('display-none');
-  bookStore.addBook(title, author);
   inputTitle.value = '';
   inputAuthor.value = '';
-  return createBookList();
+  return bookStore.addBook(title, author);
 };
-addBookBtn.addEventListener('click', updateBooks);
 
-// Create book list on page load
-createBookList();
+// run functions on window load
+window.addEventListener('load', () => {
+  document.getElementById('date-time').innerText = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()} ${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+
+  listLink.addEventListener('click', () => {
+    listBookPage.style = 'display: flex';
+    addBooksPage.style = 'display: none';
+    contactPage.style = 'display: none';
+    createHTML();
+  });
+
+  addLink.addEventListener('click', () => {
+    addBooksPage.style = 'display: flex';
+    listBookPage.style = 'display: none';
+    contactPage.style = 'display: none';
+  });
+
+  contactLink.addEventListener('click', () => {
+    listBookPage.style = 'display: none';
+    addBooksPage.style = 'display: none';
+    contactPage.style = 'display: flex';
+  });
+
+  addBookBtn.addEventListener('click', updateBooks);
+  createHTML();
+});
